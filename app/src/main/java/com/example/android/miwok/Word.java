ec91da987d8908f25177aa596dfe6606b215416b
Miguel Carvalho <miguelcansadocarvalho@gmail.com>
2018-04-13 15:05:59 +0100

    Update Word class to include image member variable + getter + constructor

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,10 +8,22 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     /**
      * @param defaultTranslation corresponds to the english word
      * @param miwokTranslation corresponds to the miwok word
+     * @param imageResourceId points to the image associated with each image
+     * */
+    public Word (String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+    /**
+     * @param defaultTranslation corresponds to the english word
+     * @param miwokTranslation corresponds to the miwok word
+     * Not all words have associated images, thus the constructor
      * */
     public Word (String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
@@ -22,4 +34,6 @@ public class Word {
     public String getmDefaultTranslation() { return mDefaultTranslation; }
 
     public String getmMiwokTranslation() { return mMiwokTranslation; }
+
+    public int getmImageResourceId() { return mImageResourceId; }
 }
