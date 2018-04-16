09f1e0034b1308e720e260524ece450253cc6e13
Miguel Carvalho <miguelcansadocarvalho@gmail.com>
2018-04-16 17:29:31 +0100

    Add code to deal with abandoning AudioFocus

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -122,6 +122,8 @@ public class PhrasesActivity extends AppCompatActivity {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mMediaPlayer = null;
+
+            mAudioManager.abandonAudioFocus(mAudioFocusChangeListener);
         }
     }
 }
